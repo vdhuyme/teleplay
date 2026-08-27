9022a018fbffb9386341a9a825450bfc2d2ced42
Võ Đức Huy <voduchuy2001@gmail.com>
2026-08-27 22:10:58 +0700

    chore: clean up tsconfig files and remove unused declarations in bot service

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,12 +1,4 @@
-import { createServer } from 'http';
 import { bot } from './bot';
-import { App } from '@teleplay/core';
-
-const port = Number(App.get('PORT', '10000'));
-const httpServer = createServer();
-httpServer.listen(port, () =>
-  console.log(`🩺 Health check server listening on port ${port}`),
-);
 
 bot.start({
   onStart: () => {
