15ebebcb17f5f7144426a1d1698a4c4cbdef2939
Võ Đức Huy <voduchuy2001@gmail.com>
2026-08-29 16:39:57 +0700

    feat: add NEXT_PUBLIC_SOCKET_PATH to environment configuration and update SOCKET_PATH handling

--- apps/player/src/utils/env.ts
+++ apps/player/src/utils/env.ts
@@ -10,8 +10,8 @@ export type Environment = (typeof Environment)[keyof typeof Environment];
 
 const ENV = {
   NODE_ENV: process.env.NODE_ENV,
-  SOCKET_PATH: process.env.SOCKET_PATH,
-  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  SOCKET_PATH: process.env.NEXT_PUBLIC_SOCKET_PATH ?? process.env.SOCKET_PATH,
+  API_URL: process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL,
   AUTH_USERNAME: process.env.AUTH_USERNAME,
   AUTH_PASSWORD: process.env.AUTH_PASSWORD,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
